4c3bcbdb2a3ac53316ed157dc8e8cf6f24d2e0bc
Un <94546447+Thanakone123@users.noreply.github.com>
2023-07-16 01:54:42 +0700

    c

--- PortfolioProject/src/components/Sections/Introduction.vue
+++ PortfolioProject/src/components/Sections/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mb-5 row align-items-center">
         <div class="col-lg-6 col-md-6 col-sm-12 mb-3 p-5">
-            <img class="img-fluid shadow-lg" src="../../assets/pictures/_TUM8803-removebg-preview.png" alt="thanakone">
+            <img class="img-fluid shadow-lg" src="../../assets/images/thanakone.png" alt="thanakone">
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12">
             <p class="mb-3" style="font-size: 16px; line-height: 1.5; color: #333;"> Hello, my name is Un. I'm a
